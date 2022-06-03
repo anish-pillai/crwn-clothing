23b15d1a648af011acfdad65679f0e45cb49020f
Anish Pillai <anishpillaimca@gmail.com>
2022-06-03 23:43:35 +0530

    redux

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+
 import {
   BackgroundImage,
   Body,
@@ -8,6 +9,7 @@ import {
 const DirectoryItem = ({ category }) => {
   const { imageUrl, title, route } = category;
   const navigate = useNavigate();
+
   const onNavigateHandler = () => navigate(route);
 
   return (
